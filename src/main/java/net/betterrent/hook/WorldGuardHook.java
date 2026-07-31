8da07ab9fa49502c474fb86fdbc8a5b75a55b243
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:33:27 +0200

    Add WorldGuardHook class for WorldGuard integration

--- src/main/java/net/betterrent/hook/WorldGuardHook.java
+++ src/main/java/net/betterrent/hook/WorldGuardHook.java
@@ -1,1 +1,19 @@
+package net.betterrent.hook;
 
+import net.betterrent.BetterRent;
+import org.bukkit.Bukkit;
+
+public class WorldGuardHook {
+
+    private final BetterRent plugin;
+
+    public WorldGuardHook(BetterRent plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean setup() {
+
+        return Bukkit.getPluginManager()
+                .getPlugin("WorldGuard") != null;
+    }
+}
