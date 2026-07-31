27f1c978fbe45636f6732696ab2dd026d67ad219
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:52:00 +0200

    Add SignListener class for event handling

---
+++ src/main/java/net/betterrent/listeners/SignListener.java
@@ -0,0 +1,18 @@
+package net.betterrent.listeners;
+
+import net.betterrent.BetterRent;
+import org.bukkit.event.Listener;
+
+public class SignListener implements Listener {
+
+
+    private final BetterRent plugin;
+
+
+    public SignListener(BetterRent plugin) {
+
+        this.plugin = plugin;
+
+    }
+
+}
