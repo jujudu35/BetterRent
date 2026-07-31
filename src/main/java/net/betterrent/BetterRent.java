0e7b9cfe23da8b1dbf1624afeaa5515470e240ed
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:39:37 +0200

    Integrate RentManager into BetterRent plugin
    Added RentManager to manage rental operations.

--- src/main/java/net/betterrent/BetterRent.java
+++ src/main/java/net/betterrent/BetterRent.java
@@ -2,6 +2,7 @@ package net.betterrent;
 
 import net.betterrent.managers.ConfigManager;
 import net.betterrent.managers.HookManager;
+import net.betterrent.managers.RentManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BetterRent extends JavaPlugin {
@@ -10,6 +11,8 @@ public final class BetterRent extends JavaPlugin {
 
     private ConfigManager configManager;
     private HookManager hookManager;
+    private RentManager rentManager;
+
 
     @Override
     public void onEnable() {
@@ -18,24 +21,32 @@ public final class BetterRent extends JavaPlugin {
 
         saveDefaultConfig();
 
+
         configManager = new ConfigManager(this);
 
         hookManager = new HookManager(this);
 
+        rentManager = new RentManager(this);
+
+
         if (!hookManager.setup()) {
+
             getLogger().severe("-------------------------------------");
             getLogger().severe("BetterRent failed to start.");
             getLogger().severe("Vault, WorldEdit or WorldGuard is missing.");
             getLogger().severe("-------------------------------------");
+
             getServer().getPluginManager().disablePlugin(this);
             return;
         }
+
 
         getLogger().info("-------------------------------------");
         getLogger().info("BetterRent enabled successfully!");
         getLogger().info("Version: " + getDescription().getVersion());
         getLogger().info("-------------------------------------");
     }
+
 
     @Override
     public void onDisable() {
@@ -44,15 +55,31 @@ public final class BetterRent extends JavaPlugin {
 
     }
 
+
     public static BetterRent getInstance() {
+
         return instance;
+
     }
+
 
     public ConfigManager getConfigManager() {
+
         return configManager;
+
     }
 
+
     public HookManager getHookManager() {
+
         return hookManager;
+
+    }
+
+
+    public RentManager getRentManager() {
+
+        return rentManager;
+
     }
 }
