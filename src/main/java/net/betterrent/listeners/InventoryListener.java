6cb2b86cf318ec3aced13430d1327f67092b487a
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:51:39 +0200

    Add InventoryListener class for event handling

---
+++ src/main/java/net/betterrent/listeners/InventoryListener.java
@@ -0,0 +1,18 @@
+package net.betterrent.listeners;
+
+import net.betterrent.BetterRent;
+import org.bukkit.event.Listener;
+
+public class InventoryListener implements Listener {
+
+
+    private final BetterRent plugin;
+
+
+    public InventoryListener(BetterRent plugin) {
+
+        this.plugin = plugin;
+
+    }
+
+}
