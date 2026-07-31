6739777584a3a9b632a02c2bd8b1ca3de1d72f8e
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:51:00 +0200

    Add BlockListener class for event handling

---
+++ src/main/java/net/betterrent/listeners/BlockListener.java
@@ -0,0 +1,18 @@
+package net.betterrent.listeners;
+
+import net.betterrent.BetterRent;
+import org.bukkit.event.Listener;
+
+public class BlockListener implements Listener {
+
+
+    private final BetterRent plugin;
+
+
+    public BlockListener(BetterRent plugin) {
+
+        this.plugin = plugin;
+
+    }
+
+}
