6a4c8b9feae7d9bdbd6b5c62532c64c8309880b2
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:32:55 +0200

    Add WorldEditHook class for WorldEdit integration

--- src/main/java/net/betterrent/hook/WorldEditHook.java
+++ src/main/java/net/betterrent/hook/WorldEditHook.java
@@ -1,1 +1,31 @@
+package net.betterrent.hook;
 
+import net.betterrent.BetterRent;
+import com.sk89q.worldedit.bukkit.WorldEditPlugin;
+import org.bukkit.Bukkit;
+
+public class WorldEditHook {
+
+    private final BetterRent plugin;
+    private WorldEditPlugin worldEdit;
+
+    public WorldEditHook(BetterRent plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean setup() {
+
+        if (Bukkit.getPluginManager().getPlugin("WorldEdit") == null) {
+            return false;
+        }
+
+        worldEdit = (WorldEditPlugin)
+                Bukkit.getPluginManager().getPlugin("WorldEdit");
+
+        return worldEdit != null;
+    }
+
+    public WorldEditPlugin getWorldEdit() {
+        return worldEdit;
+    }
+}
