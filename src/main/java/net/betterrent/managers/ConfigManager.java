d935c05a8f17893fd36fc48c42b894473aadb976
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:27:34 +0200

    Implement ConfigManager class for BetterRent plugin

--- src/main/java/net/betterrent/managers/ConfigManager.java
+++ src/main/java/net/betterrent/managers/ConfigManager.java
@@ -1,1 +1,16 @@
+package net.betterrent.managers;
 
+import net.betterrent.BetterRent;
+
+public class ConfigManager {
+
+    private final BetterRent plugin;
+
+    public ConfigManager(BetterRent plugin) {
+        this.plugin = plugin;
+    }
+
+    public BetterRent getPlugin() {
+        return plugin;
+    }
+}
