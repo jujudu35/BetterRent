5412460a784987f23240f465cc5c6f43bd14752a
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:27:52 +0200

    Create HookManager class for BetterRent plugin
    Implemented HookManager class to manage hooks for BetterRent plugin.

--- src/main/java/net/betterrent/managers/HookManager.java
+++ src/main/java/net/betterrent/managers/HookManager.java
@@ -1,1 +1,20 @@
+package net.betterrent.managers;
 
+import net.betterrent.BetterRent;
+
+public class HookManager {
+
+    private final BetterRent plugin;
+
+    public HookManager(BetterRent plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean setup() {
+        return true;
+    }
+
+    public BetterRent getPlugin() {
+        return plugin;
+    }
+}
