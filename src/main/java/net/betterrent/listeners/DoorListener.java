7f1f76e45b2e4a9a48090e54c28b20ab4b8ae798
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:51:21 +0200

    Add DoorListener class for event handling

---
+++ src/main/java/net/betterrent/listeners/DoorListener.java
@@ -0,0 +1,18 @@
+package net.betterrent.listeners;
+
+import net.betterrent.BetterRent;
+import org.bukkit.event.Listener;
+
+public class DoorListener implements Listener {
+
+
+    private final BetterRent plugin;
+
+
+    public DoorListener(BetterRent plugin) {
+
+        this.plugin = plugin;
+
+    }
+
+}
