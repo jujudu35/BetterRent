9d71bd274897f7beacb17669534b6fef02674ebd
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:32:31 +0200

    Add VaultHook for Vault economy integration
    Implement VaultHook class to integrate with Vault economy.

--- src/main/java/net/betterrent/hook/VaultHook.java
+++ src/main/java/net/betterrent/hook/VaultHook.java
@@ -1,1 +1,38 @@
+package net.betterrent.hook;
 
+import net.betterrent.BetterRent;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+public class VaultHook {
+
+    private final BetterRent plugin;
+    private Economy economy;
+
+    public VaultHook(BetterRent plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean setup() {
+
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+
+        RegisteredServiceProvider<Economy> rsp =
+                Bukkit.getServicesManager().getRegistration(Economy.class);
+
+        if (rsp == null) {
+            return false;
+        }
+
+        economy = rsp.getProvider();
+
+        return economy != null;
+    }
+
+    public Economy getEconomy() {
+        return economy;
+    }
+}
