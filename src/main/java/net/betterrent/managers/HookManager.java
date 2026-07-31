8ff7d20c2c9cb0d643e32ade469716f209169bed
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:33:52 +0200

    Initialize hooks for Vault, WorldEdit, and WorldGuard

--- src/main/java/net/betterrent/managers/HookManager.java
+++ src/main/java/net/betterrent/managers/HookManager.java
@@ -1,20 +1,46 @@
 package net.betterrent.managers;
 
 import net.betterrent.BetterRent;
+import net.betterrent.hook.*;
 
 public class HookManager {
 
     private final BetterRent plugin;
 
+    private VaultHook vaultHook;
+    private WorldEditHook worldEditHook;
+    private WorldGuardHook worldGuardHook;
+
+
     public HookManager(BetterRent plugin) {
+
         this.plugin = plugin;
+
+        vaultHook = new VaultHook(plugin);
+        worldEditHook = new WorldEditHook(plugin);
+        worldGuardHook = new WorldGuardHook(plugin);
     }
+
 
     public boolean setup() {
-        return true;
+
+        return vaultHook.setup()
+                && worldEditHook.setup()
+                && worldGuardHook.setup();
     }
 
-    public BetterRent getPlugin() {
-        return plugin;
+
+    public VaultHook getVaultHook() {
+        return vaultHook;
+    }
+
+
+    public WorldEditHook getWorldEditHook() {
+        return worldEditHook;
+    }
+
+
+    public WorldGuardHook getWorldGuardHook() {
+        return worldGuardHook;
     }
 }
