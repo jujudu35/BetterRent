8a49329e1e054e5df31a84e61ec3bddfc7480a69
jujudu35 <julien2002.milo@hotmail.fr>
2026-07-31 18:20:51 +0200

    Implement BetterRent plugin with configuration management

--- src/main/java/net/betterrent/BetterRent.java
+++ src/main/java/net/betterrent/BetterRent.java
@@ -1,1 +1,58 @@
+package net.betterrent;
 
+import net.betterrent.managers.ConfigManager;
+import net.betterrent.managers.HookManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterRent extends JavaPlugin {
+
+    private static BetterRent instance;
+
+    private ConfigManager configManager;
+    private HookManager hookManager;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        saveDefaultConfig();
+
+        configManager = new ConfigManager(this);
+
+        hookManager = new HookManager(this);
+
+        if (!hookManager.setup()) {
+            getLogger().severe("-------------------------------------");
+            getLogger().severe("BetterRent failed to start.");
+            getLogger().severe("Vault, WorldEdit or WorldGuard is missing.");
+            getLogger().severe("-------------------------------------");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        getLogger().info("-------------------------------------");
+        getLogger().info("BetterRent enabled successfully!");
+        getLogger().info("Version: " + getDescription().getVersion());
+        getLogger().info("-------------------------------------");
+    }
+
+    @Override
+    public void onDisable() {
+
+        getLogger().info("BetterRent disabled.");
+
+    }
+
+    public static BetterRent getInstance() {
+        return instance;
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public HookManager getHookManager() {
+        return hookManager;
+    }
+}
