a4ef8b28d92323bdfee1d0a0758f6e0c0b2f5d8f
jujudu35 <julien2002.milo@hotmail.fr>
2026-08-01 12:35:43 +0200

    Refactor HouseStorage to include file handling

--- src/main/java/net/betterrent/storage/HouseStorage.java
+++ src/main/java/net/betterrent/storage/HouseStorage.java
@@ -11,133 +11,250 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
-
 public class HouseStorage {
-public void save() {
 
 
-    for (Map.Entry<String, RentHouse> entry :
-            plugin.getRentManager()
-                    .getHouses()
-                    .entrySet()) {
+    private final BetterRent plugin;
+
+    private File file;
+
+    private FileConfiguration config;
 
 
-        String path = "houses." + entry.getKey();
 
-        RentHouse house = entry.getValue();
+    public HouseStorage(BetterRent plugin) {
+
+        this.plugin = plugin;
+
+        load();
+
+    }
 
 
-        config.set(path + ".name", house.getName());
 
-        config.set(path + ".price", house.getPricePerDay());
+    private void load() {
 
 
-        if (house.getOwner() != null) {
+        file = new File(
+                plugin.getDataFolder(),
+                "houses.yml"
+        );
+
+
+        if (!file.exists()) {
+
+            try {
+
+                file.createNewFile();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+        }
+
+
+        config = YamlConfiguration.loadConfiguration(file);
+
+    }
+
+
+
+
+
+    public void save() {
+
+
+        for (Map.Entry<String, RentHouse> entry :
+                plugin.getRentManager()
+                        .getHouses()
+                        .entrySet()) {
+
+
+            String path = "houses." + entry.getKey();
+
+            RentHouse house = entry.getValue();
+
+
 
             config.set(
-                    path + ".owner",
-                    house.getOwner().toString()
+                    path + ".name",
+                    house.getName()
+            );
+
+
+            config.set(
+                    path + ".price",
+                    house.getPricePerDay()
+            );
+
+
+
+            if (house.getOwner() != null) {
+
+                config.set(
+                        path + ".owner",
+                        house.getOwner().toString()
+                );
+
+            }
+
+
+
+            config.set(
+                    path + ".expire",
+                    house.getExpireTime()
+            );
+
+
+
+            saveLocation(
+                    house.getPos1(),
+                    path + ".pos1"
+            );
+
+
+            saveLocation(
+                    house.getPos2(),
+                    path + ".pos2"
+            );
+
+
+
+            config.set(
+                    path + ".region",
+                    house.getWorldGuardRegion()
+            );
+
+
+
+            config.set(
+                    path + ".trusted",
+                    house.getTrustedPlayers()
+                            .stream()
+                            .map(UUID::toString)
+                            .toList()
+            );
+
+
+
+            // Permissions locataire
+
+            config.set(
+                    path + ".permissions.open-doors",
+                    house.canOpenDoors()
+            );
+
+            config.set(
+                    path + ".permissions.open-trapdoors",
+                    house.canOpenTrapdoors()
+            );
+
+            config.set(
+                    path + ".permissions.open-fence-gates",
+                    house.canOpenFenceGates()
+            );
+
+            config.set(
+                    path + ".permissions.place-blocks",
+                    house.canPlaceBlocks()
+            );
+
+            config.set(
+                    path + ".permissions.break-blocks",
+                    house.canBreakBlocks()
+            );
+
+            config.set(
+                    path + ".permissions.open-chests",
+                    house.canOpenChests()
+            );
+
+            config.set(
+                    path + ".permissions.open-barrels",
+                    house.canOpenBarrels()
+            );
+
+            config.set(
+                    path + ".permissions.open-shulkers",
+                    house.canOpenShulkers()
+            );
+
+            config.set(
+                    path + ".permissions.use-furnaces",
+                    house.canUseFurnaces()
+            );
+
+            config.set(
+                    path + ".permissions.use-anvils",
+                    house.canUseAnvils()
+            );
+
+            config.set(
+                    path + ".permissions.use-crafting",
+                    house.canUseCrafting()
+            );
+
+            config.set(
+                    path + ".permissions.use-enchanting",
+                    house.canUseEnchanting()
             );
 
         }
 
 
-        config.set(
-                path + ".expire",
-                house.getExpireTime()
-        );
 
+        try {
 
-        saveLocation(
-                house.getPos1(),
-                path + ".pos1"
-        );
+            config.save(file);
 
+        } catch (IOException e) {
 
-        saveLocation(
-                house.getPos2(),
-                path + ".pos2"
-        );
+            e.printStackTrace();
 
-
-        config.set(
-                path + ".region",
-                house.getWorldGuardRegion()
-        );
-
-
-        config.set(
-                path + ".trusted",
-                house.getTrustedPlayers()
-                        .stream()
-                        .map(UUID::toString)
-                        .toList()
-        );
-
-
-        config.set(path + ".permissions.open-doors",
-                house.canOpenDoors());
-
-        config.set(path + ".permissions.open-trapdoors",
-                house.canOpenTrapdoors());
-
-        config.set(path + ".permissions.open-fence-gates",
-                house.canOpenFenceGates());
-
-        config.set(path + ".permissions.place-blocks",
-                house.canPlaceBlocks());
-
-        config.set(path + ".permissions.break-blocks",
-                house.canBreakBlocks());
-
-        config.set(path + ".permissions.open-chests",
-                house.canOpenChests());
-
-        config.set(path + ".permissions.open-barrels",
-                house.canOpenBarrels());
-
-        config.set(path + ".permissions.open-shulkers",
-                house.canOpenShulkers());
-
-        config.set(path + ".permissions.use-furnaces",
-                house.canUseFurnaces());
-
-        config.set(path + ".permissions.use-anvils",
-                house.canUseAnvils());
-
-        config.set(path + ".permissions.use-crafting",
-                house.canUseCrafting());
-
-        config.set(path + ".permissions.use-enchanting",
-                house.canUseEnchanting());
+        }
 
     }
 
 
-    try {
 
-        config.save(file);
 
-    } catch (IOException e) {
 
-        e.printStackTrace();
+    private void saveLocation(Location loc, String path) {
+
+
+        if (loc == null) {
+
+            return;
+
+        }
+
+
+        config.set(
+                path + ".world",
+                loc.getWorld().getName()
+        );
+
+
+        config.set(
+                path + ".x",
+                loc.getX()
+        );
+
+
+        config.set(
+                path + ".y",
+                loc.getY()
+        );
+
+
+        config.set(
+                path + ".z",
+                loc.getZ()
+        );
 
     }
-
-} // <-- FIN DE save()
-
-
-
-// ICI seulement
-private void saveLocation(Location loc, String path) {
-
-    if (loc == null) {
-        return;
-    }
-
-    config.set(path + ".world", loc.getWorld().getName());
-    config.set(path + ".x", loc.getX());
-    config.set(path + ".y", loc.getY());
-    config.set(path + ".z", loc.getZ());
 
 }
